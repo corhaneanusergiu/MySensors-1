69f44ba6ea41da8289fdd834e6d8e266cd7ef69a
Mark Swift <mark@useprofile.com>
2017-01-17 10:13:03 +0000

    Updates

--- Custom/Gate/Gate.ino
+++ Custom/Gate/Gate.ino
@@ -2,20 +2,21 @@
   REVISION HISTORY
   Created by Mark Swift
   V1.2 - Added sleep time to force daily update
-  V1.3 - Deactivate pullup on setup and added oldValue logic
+  V1.3 - Deactivate pullup on setup and added old_value logic
   V1.4 - Added daily wakeup to sleep
+  V1.5 - Code cleanup
 */
 
 //*** EXTERNAL LIBRARIES **********************************
 
 #include <Vcc.h>
 
-//*** MY SENSORS ******************************************
+//*** MYSENSORS *******************************************
 
-// Enable debug prints to serial monitor
+// Enable debug prints
 // #define MY_DEBUG
 
-// For use with 1Mhz modules
+// Lower serial speed if using 1Mhz clock
 // #define MY_BAUD_RATE 9600
 
 #define MY_NODE_ID 4
@@ -26,109 +27,101 @@
 #define MY_RADIO_NRF24
 // #define MY_RADIO_RFM69
 
-// Channel furthest away from Wifi
+// Override RF24L01 channel number
 // #define MY_RF24_CHANNEL 125
 
-// For crappy PA+LNA module
+// Override RF24L01 module PA level
 // #define MY_RF24_PA_LEVEL RF24_PA_LOW
+
+// Override RF24L01 datarate
+// #define MY_RF24_DATARATE RF24_250KBPS
 
 // Enabled repeater feature for this node
 // #define MY_REPEATER_FEATURE
 
 #include <MySensors.h>
 
-//*** CONFIG **********************************************
+// *** SKETCH CONFIG **************************************
 
 #define SKETCH_NAME "Door Sensor"
 #define SKETCH_MAJOR_VER "1"
-#define SKETCH_MINOR_VER "4"
+#define SKETCH_MINOR_VER "5"
 
+// Define the sensor child IDs
 #define CHILD_ID1 1
 #define CHILD_ID2 2
 #define CHILD_ID3 3
 
+// Define the message formats
 MyMessage msg(CHILD_ID1, V_TRIPPED);
 MyMessage msg2(CHILD_ID2, V_CUSTOM);
 MyMessage msg3(CHILD_ID3, V_CUSTOM);
 
-// Arduino Digital I/O pin for button or reed switch
+// *** SENSORS CONFIG *************************************
+
+// Digital I/O pin for button or reed switch
 #define PRIMARY_BUTTON_PIN 3
 
-// Store the old value for comparison
-int oldValue = -1;
+// Debounce using a small sleep
+#define DEBOUNCE_SLEEP 5
 
 // Sleep timer in milliseconds
 #define SLEEP_IN_MS 86400000
 
-// Calibrate and setup voltage parameters
-const float VccMin = 2.0 * 0.9; // Minimum expected Vcc level, in Volts. Example for 2xAA Alkaline.
-const float VccMax = 2.0 * 1.5; // Maximum expected Vcc level, in Volts. Example for 2xAA Alkaline.
-const float VccCorrection = 2.88 / 2.82; // Measured Vcc by multimeter divided by reported Vcc
-Vcc vcc(VccCorrection);
+// Store the old value for comparison
+int old_value = -1;
 
-//*** BEGIN ***********************************************
+// Minimum expected vcc level, in volts (2xAA alkaline)
+const float vcc_min = 2.0 * 0.9;
+// Maximum expected vcc level, in volts (2xAA alkaline)
+const float vcc_max = 2.0 * 1.5;
+// Measured vcc by multimeter divided by reported vcc
+const float vcc_correction = 2.88 / 2.82;
+Vcc vcc(vcc_correction);
 
-void setup()
-{
+// *** BEGIN **********************************************
+
+void setup() {
   // Setup the contact sensor
   pinMode(PRIMARY_BUTTON_PIN, INPUT);
-
-  // Activate or deactivate the internal pull-up / High if using internal resistor, low is using external.
+  // Activate or deactivate the internal pull-up - high if using internal resistor or low if using external
   digitalWrite(PRIMARY_BUTTON_PIN, LOW);
 }
 
 void presentation() {
   // Send the sketch version information to the gateway and controller
   sendSketchInfo(SKETCH_NAME, SKETCH_MAJOR_VER "." SKETCH_MINOR_VER);
-
-  // Register binary input sensor to sensor_node (they will be created as child devices).
-  // You can use S_DOOR, S_MOTION or S_LIGHT here depending on your usage.
-  // If S_LIGHT is used, remember to update variable type you send in. See "msg" above.
+  // Register all sensors to the gateway
   present(CHILD_ID1, S_DOOR);
   present(CHILD_ID2, S_CUSTOM);
   present(CHILD_ID3, S_CUSTOM);
 }
 
-// Loop will iterate on changes on the BUTTON_PINs
-void loop()
-{
-#ifdef MY_DEBUG
-  Serial.print("Waking");
-#endif
-
-  // Short delay to allow inputs to settle
-  sleep(5);
-
+void loop() {
+  // Debouce using sleep
+  sleep(DEBOUNCE_SLEEP);
   int value = digitalRead(PRIMARY_BUTTON_PIN);
-
-  // If value has changed from last transmission, send the updated value
-  if (value != oldValue) {
+  // If value has changed send the updated value
+  if (value != old_value) {
     send(msg.set(value == HIGH ? 0 : 1 ));
-    oldValue = value;
+    old_value = value;
   }
-
-  float v = vcc.Read_Volts();
+  // Read battery voltage
+  float battery_voltage = vcc.Read_Volts();
+  // Calculate battery percentage
+  float battery_percent = vcc.Read_Perc(vcc_min, vcc_max);
 #ifdef MY_DEBUG
-  Serial.print("VCC = ");
-  Serial.print(v);
+  Serial.print("Battery = ");
+  Serial.print(battery_voltage);
   Serial.println(" Volts");
-#endif
-
-  float p = vcc.Read_Perc(VccMin, VccMax);
-#ifdef MY_DEBUG
-  Serial.print("VCC = ");
-  Serial.print(p);
+  Serial.print("Battery = ");
+  Serial.print(battery_percent);
   Serial.println("%");
 #endif
-
-  send(msg2.set(v, 2));
-  send(msg3.set(p, 1));
-  sendBatteryLevel(p);
-
-  // Sleep until something either interupt or timer reaches zero
-
-#ifdef MY_DEBUG
-  Serial.print("Sleeping");
-#endif
+  // Send battery readings
+  send(msg2.set(battery_voltage, 2));
+  send(msg3.set(battery_percent, 1));
+  sendBatteryLevel(battery_percent);
+  // Sleep until either interupt change or timer reaches zero
   smartSleep(PRIMARY_BUTTON_PIN - 2, CHANGE, SLEEP_IN_MS);
 }
